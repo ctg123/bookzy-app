e449e5bcddb4b808dcac01752237c0fea41c337f
ctg123 <chaanceg123@gmail.com>
2021-02-08 22:11:50 -0800

    modified demoApplication.java

--- src/main/java/com/bookzy/demoApplication/DemoApplication.java
+++ src/main/java/com/bookzy/demoApplication/DemoApplication.java
@@ -3,11 +3,17 @@ package com.bookzy.demoApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 @ComponentScan("com.*") // This allows for the main application to scan all com packages included.
 public class DemoApplication {
 
+	@RequestMapping("/home")
+	@ResponseBody
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
