fc063051825e05d093c1f0c6be77e17ea1a3f3a3
ctg123 <chaanceg123@gmail.com>
2021-02-08 21:37:59 -0800

    added com package for getMapping in the controller class.

--- src/main/java/com/bookzy/controllers/HomeController.java
+++ src/main/java/com/bookzy/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.bookzy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
